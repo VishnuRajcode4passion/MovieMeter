60e8e8dcc012efd53099a267817738e583538d23
Machine 4 <gokuldasvattamala@gmail.com>
2016-05-10 10:43:51 +0530

    no message

--- app/src/main/java/com/example/machine2/moviesss/LoginActivity.java
+++ app/src/main/java/com/example/machine2/moviesss/LoginActivity.java
@@ -1,18 +1,37 @@
 package com.example.machine2.moviesss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
+    ImageView loginButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.content_login);
+        loginButton = (ImageView)findViewById(R.id.imageView6);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                System.out.println("login ");
+                Intent intent;
+                intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
